fc04eb1ac4e8d0de4b8e6f2e4748b94bddd2d81c
karan-tewari <ktewari601@gmail.com>
2020-08-25 23:03:58 +0530

    form

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import Home from './components/Home';
 import AddLocation from './components/AddLocation'
+import LocationTimings from './components/LocationTimings';
 
 function App() {
   return (
     <div className="App">
-      <AddLocation />
+      <LocationTimings />
     </div>
   );
 }
